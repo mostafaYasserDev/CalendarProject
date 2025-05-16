cf5b44509cc80a29fc5686fa7743f5cd9dfc02fc
mostafaYasserKamel <moustafa0yasser123@gmail.com>
2025-05-16 20:13:22 +0300

    commit

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://calendar-project.vercel.app/"],
     credentials: true,
   })
 );
